77e1ea35086fc5e10b9d0dd371d983d427b27de3
a <ari.sajadi@gmail.com>
2019-05-09 16:52:02 +0200

    add shopping slidshow

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-
 document.addEventListener( "DOMContentLoaded",
  function(){
-  //var d = new Date();
-
-   //document.getElementById("c1").innerHTML = d.toDateString();
-   currekttime();
+ // ------------------------------------------------------------ show currenttime
+   currenttime();
   }
+ // ------------------------------------------------------------ end show currenttime
 );
-function currekttime() {
+ // ------------------------------------------------------------ currenttime
+function currenttime() {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
-  m = lenghtsec(m);
-  s = lenghtsec(s);
-  document.getElementById('c1').innerHTML = today.toDateString()+" - "+
+  m = lenghtchar(m);
+  s = lenghtchar(s);
+  document.getElementById('colock').innerHTML = today.toDateString()+" - "+
   h + ":" + m + ":" + s;
-  var t = setTimeout(currekttime, 500);
+  var t = setTimeout(currenttime, 500);
 }
-function lenghtsec(i) {
+function lenghtchar(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
+ // ------------------------------------------------------------ end currenttime
+ // ------------------------------------------------------------ slide show
+
+ // ------------------------------------------------------------ en slide show
 
 
 
-
-
-
+ // ------------------------------------------------------------ fixed menu
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
   document.getElementById("mySidepanel").style.width = "250px";
@@ -37,3 +38,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
+ // ------------------------------------------------------------ end fixed menu
+ // ------------------------------------------------------------ Shopping slideshow
+ $(document).ready(function(){
+  $('.shopping-slidshow.big').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.shopping-slidshow.small').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true
+  });
+}); 
+ // ------------------------------------------------------------ end hopping slideshow
