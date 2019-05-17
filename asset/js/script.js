c84968591d523f7ffa5e99c76c7881fd3bbbb0c4
a <ari.sajadi@gmail.com>
2019-05-17 14:31:56 +0200

    display none to block OpenBut

--- asset/js/script.js
+++ asset/js/script.js
@@ -31,8 +31,9 @@ function openNav() {
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
+
   document.getElementById("mySidepanel").style.width = "0";
-  document.getElementsByClassName("openbtn")[0].style.display = "fixed";
+  document.getElementsByClassName("openbtn")[0].style.display = "block";
 }
  // ------------------------------------------------------------ end fixed menu
  // ------------------------------------------------------------ Shopping slideshow
